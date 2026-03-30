87e995171346874a5dc6401a6d469e54c6f4a129
Pasula Laxmi Narasimha Raju <lnraju366@gmail.com>
2026-03-30 19:31:50 +0530

    Time: 15 ms (20.03%) | Memory: 46.3 MB (30.15%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String res = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+
+        int i=0,j=res.length()-1;
+
+        while(i<j){
+            if(res.charAt(i) != res.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
