aab832a511e525d642de8f2193047788fe063551
Pasula Laxmi Narasimha Raju <lnraju366@gmail.com>
2026-09-08 23:17:50 +0530

    Time: 368 ms (15.43%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-
-Select max(salary) as SecondHighestSalary from Employee
-where salary < (Select max(salary) from Employee);
+Select max(salary) as SecondHighestSalary
+from Employee
+where salary<(Select max(salary) from Employee);
